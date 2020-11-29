fb15d3c0fe248ced5648afb24541c13d8610fe92
Daria Korytova <dk17march@gmail.com>
2020-11-29 18:48:57 +0300

    actions in projects and todos controllers

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,33 @@
 class ProjectsController < ApplicationController
+  before_action :find_project, only: :index
+
   def index
-    render json: { message: "ok" }
+    # render json: { message: "ok" }
+    @projects = Project.all
+    @todos = @project.todos
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title)
+  end
+
+  def find_project
+    @project = Project.find(params[:id])
   end
 end
